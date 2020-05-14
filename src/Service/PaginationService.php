daf0390f521f933248d77c4e80ee47135f1ac0de
yassine hassak <yacinehassak@gmail.com>
2020-05-14 02:29:05 +0100

    Mise en place de la logique de pagination dans l'adminstration

---
+++ src/Service/PaginationService.php
@@ -0,0 +1,117 @@
+<?php
+
+namespace App\Service;
+
+use Twig\Environment;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\RequestStack;
+
+
+class PaginationService {
+private $entityClass ;
+private $limit = 10 ;
+private $currentPage = 1 ;
+private $manager ;
+private $twig ;
+private $route ;
+private $templatePath ;
+
+public function __construct(EntityManagerInterface $manager , Environment $twig, RequestStack $request ,
+$templatePath) {
+    $this->route = $request->getCurrentRequest()->attributes->get('_route') ;
+    $this->manager = $manager ;
+    $this->twig = $twig ;
+    $this->templatePath = $templatePath ;
+}
+
+public function setTemplatePath($templatePath){
+    $this->setTemplatePath = $templatePath ;
+
+    return $this ; 
+}
+
+public function getTemplatePath() {
+    return $this->templatePath ;
+}
+
+public function setRoute($route){
+    $this->route = $route ;
+
+    return $this ;
+}
+
+public function getRoute(){
+    return $route ;
+}
+
+public function display() {
+    $this->twig->display($this->templatePath,[
+        'page' =>  $this->currentPage ,
+        'pages' => $this->getPages() ,
+        'route' => $this->route
+    ]) ;
+}
+public function getPages(){
+    if(empty($this->entityClass)){
+        throw new \Exception("Vous n'avez pas spécifié l'entité sur laquelle nous devons paginer ! Utiliser la méthode setEntityClass() de votre objet PaginationService");
+    }
+    // Connaitre le total des enregistrements de la table
+    $repo = $this->manager->getRepository($this->entityClass) ;
+    $total = count($repo->findAll()) ;
+
+    // Faire la division, l'arrondi et le renvoyer
+    $pages = ceil($total / $this->limit) ;
+
+    return $pages ;
+}
+
+public function getData() {
+    if(empty($this->entityClass)){
+        throw new \Exception("Vous n'avez pas spécifié l'entité sur laquelle nous devons paginer !
+        Utilisez la méthode setEntityClass() de votre objet PaginationService") ;
+    }
+    // 1) Calculer l'offest
+    $offset = $this->currentPage * $this->limit - $this->limit ; 
+
+    // 2) Demander au repository de trouver les élements
+    $repo = $this->manager->getRepository($this->entityClass) ;
+    $data = $repo->findBy([],[],$this->limit, $offset) ;
+
+    // 3) Renvoyer les élements en question
+    return $data ;
+
+}
+
+public function setPage($page){
+    $this->currentPage = $page ;
+
+    return $this ;
+}
+
+public function getPage(){
+    return $this->currentPage ;
+}
+
+public function setLimit($limit){
+    $this->limit = $limit ;
+
+    return $this ;
+}
+
+public function getLimit(){
+    return $this->limit ;
+}
+
+public function setEntityClass($entityClass){
+    $this->entityClass = $entityClass ;
+
+    return $this ;
+}
+
+public function getEntityClass(){
+    return $this->entityClass ;
+}
+
+
+}
+?>
